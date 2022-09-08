23e581a79e580c6e029c5da7648da39a067dfa29
saba1024 <sabatamrat24@gmail.com>
2022-09-08 14:08:36 -0700

    print f C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-/*main printing*/
+/**
+ * main printing
+ *
+ * Return: Always 0 (Successful)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

c192b5fa0facb786663b8ff5b81e1fabc16623b5
saba1024 <sabatamrat24@gmail.com>
2022-09-13 14:50:02 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - function to check for a sign of a number
+ * @c: is the int that will use foor the argument of the function
+ * Return: 0
+ */
+int pprint_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if ( n < 0)
+	{
+		_putchhar( '-')
+			return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		retuurn (0);
+	}
+}

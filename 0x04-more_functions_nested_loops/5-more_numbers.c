916d08921d120e92728a8fa29a55459129ca93b0
saba1024 <sabatamrat24@gmail.com>
2022-09-15 13:49:54 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.c"
+
+/**
+ * void more_numbers - prints numbers to 14
+ *
+ * Return: return nothing
+ */
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 10; n1++)
+	{
+		for (n2 =0; n2 <= 14; n2++)
+		{
+			if (n2 > 9)
+			{
+				_putchar((n2 / 10) + '0');
+			}
+			_putchar((n2 % 10) + '0');
+		}
+		_putchar(10);
+	}
+}

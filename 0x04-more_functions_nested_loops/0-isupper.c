0af272b65ea6903b2b1073a473f718d7abeb11be
saba1024 <sabatamrat24@gmail.com>
2022-09-15 12:39:30 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c;
+
+	if
+	{
+	c = "A - Z";
+	_putchar'1';
+	}
+	else if
+	{
+	c = "a - z";
+	_putchar'0'
+	}
+	return (0)
+}

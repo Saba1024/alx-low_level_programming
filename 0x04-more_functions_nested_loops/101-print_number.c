d98eb98b258ef01ad5a34a7268ae587ecc3b521a
saba1024 <sabatamrat24@gmail.com>
2022-09-16 15:05:18 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,7 +3,6 @@
 /**
  * print_number - prints numbers
  * @n: number to be printed
- * Return: void
  */
 
 void print_number(int n)
@@ -25,12 +24,12 @@ void print_number(int n)
 
 	while (j > 9)
 	{
-		j /=10;
+		j /= 10;
 		count *= 10;
 	}
 
 	for (; count >= 1; count /= 10)
 	{
-		_putchar(((i / count) % 10) + 46);
+		_putchar(((i / count) % 10) + 48);
 	}
 }

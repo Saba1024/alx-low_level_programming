68bf13cbc3aea326ed4aad4be333708072e7daaa
saba1024 <sabatamrat24@gmail.com>
2022-09-16 07:30:06 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,23 +8,19 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
-		}
-		else if
-		{
-			for (i == n)
-				_putchar('92');
+			for (j = 0; j <= i; j++)
+				_putchar(' ');
+			_putchar(92);
 			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }

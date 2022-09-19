192f60cfce5c3556fecf134b48afa9f18ef04f8a
saba1024 <sabatamrat24@gmail.com>
2022-09-19 14:09:01 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,43 +8,28 @@
  *
  * Return: Always 0
  */
-
 int main(void)
 {
-	char password(84);
-	int index = 0, sum = 0, diff_half1, diff_half2;
+	int pwd[100];
+	int i, sum, n;
 
-	srand(time(0));
-	while (sum < 2772)
-	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
-	}
-	password[index] = '\0';
+	sum = 0;
 
-	if (sum != 2772)
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
 	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-		if ((sum - 2772) % 2 != 0)
-			diff_half1++;
-		for (index = 0; password[index]; index++)
+		pwd[i] = rand() % 78;
+		sum += (pwd[i] + '0');
+		putchar(pwd[i] + '0');
+		if ((2772 - sum) - '0' < 78)
 		{
-			if (password[index] >= (33 + diff_half1))
-			{
-				password[index] -= diff_half1;
-				break;
-			}
-		}
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half2))
-			{
-				password[index] -= diff_half2;
-				break;
-			}
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
 		}
 	}
-	printf("%s", password);
+
 	return (0);
 }

ab593d2e402af98c40a61eb5882ea98eaa6f0349
saba1024 <sabatamrat24@gmail.com>
2022-09-20 14:03:44 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,42 +1,48 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
- *
  * Return: the int converted from the string
  */
 
 int _atoi(char *s)
 {
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
+	int num, len, numcount, mult, x, neg;
 
-	while (s[c])
+	len = 0;
+	x = 0;
+	numcount = 0;
+	mult = 1;
+	neg = 1;
+	num = 0;
+
+	while (s[len] != '\0')
 	{
-		if (s[c] == 45)
+		if (s[len] >= '0' && s[len] <= '9')
 		{
-			min *= -1;
+			numcount++;
+			if (!(s[len + 1] >= '0' && s[len + 1] <= '9'))
+				break;
 		}
-
-		while (s[c] >= 48 && s[c] <= 57)
-		{
-			isi = 1;
-			ni = (ni * 10) + (s[c] -'0');
-			c++;
-		}
-
-		if (/isi == 1)
-		{
-			break;
-		}
-
-		c++;
+		len++;
 	}
 
-	ni *= min;
-	return (ni);
+	for (; numcount > 1; numcount--)
+	{
+		mult *= 10;
+	}
+
+	for (; x <= len; x++)
+	{
+		if (s[x] == '-')
+			neg *= -1;
+		else if (s[x] <= '9' && s[x] >= '0')
+		{
+			num += (s[x] - '0') * mult * neg;
+			mult /= 10;
+		}
+	}
+	
+	return (num);
 }

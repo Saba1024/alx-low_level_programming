7321e10606f2ec27ef63ed2fbef3182b0594b2b1
saba1024 <sabatamrat24@gmail.com>
2022-09-22 14:42:44 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,4 +2,18 @@
 
 /**
  * rot13 - a function that encodes a string using rot13.
+ * @str: string to be encoded
  *
+ * Return: address of the encoded string
+ */
+char *rot13(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		str[i] = transform_2(str[i]);
+		i++;
+	}
+	return (str);
+}

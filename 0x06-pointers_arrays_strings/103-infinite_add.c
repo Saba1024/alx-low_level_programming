4792d9747f998406e9d5ae768a5fff61b89c582b
saba1024 <sabatamrat24@gmail.com>
2022-09-22 16:35:29 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,10 +6,10 @@
  * @n2: Second number
  * @r: result buffer
  * @size_r: result buffer size
+ *
  * Return: 0 if the result can not be stored in r,
  * otherwise a pointer to the result
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, k, l, m, n;

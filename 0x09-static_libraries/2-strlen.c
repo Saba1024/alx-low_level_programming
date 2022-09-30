558cd0363949baa3bab53a90a815151ab3606c73
saba1024 <sabatamrat24@gmail.com>
2022-09-30 13:04:01 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: string pointer
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}

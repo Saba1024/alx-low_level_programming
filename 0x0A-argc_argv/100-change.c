4d76cff1eebd7a69f13b46b320d4fffe794d31ea
saba1024 <sabatamrat24@gmail.com>
2022-09-30 12:08:16 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,15 +7,15 @@
  * make change for an amount of money
  * @argc: number of arguments
  * @argv: array of arguments
-
+ *
  * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
 	int num, j, result;
 	int coins[] = {25, 10, 5, 2, 1};
-	
-	if(argc != 2)
+
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);

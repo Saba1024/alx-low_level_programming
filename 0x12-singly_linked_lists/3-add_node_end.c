a1138a8b1851c84a8467c6cb91da405eb334108c
saba1024 <sabatamrat24@gmail.com>
2022-10-20 11:38:25 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,7 +1,7 @@
-#include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
+int _strlen(const char *s);
 /**
  * add_node_end - adds a new node at the end of a linked list
  * @head: double pointer to the list_t list
@@ -9,33 +9,43 @@
  *
  * Return: address of the new element, or NULL if it failed
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
-	list_t *temp = *head;
-	unsigned int len = 0;
-
-	while (str[len])
-		len++;
-
+	list_t *lastNode;
+	
 	new = malloc(sizeof(list_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = len;
+	new->len = _strlen(str);
 	new->next = NULL;
-
 	if (*head == NULL)
+		*head = new;
+
+	else
 	{
-		head = new;
-		return (new);
+		lastNode = *head;
+		while (lastNode->next != NULL)
+			lastNode = lastNode->next;
+		lastNode->next = new;
 	}
-
-	while (temp->next)
-		temp = temp->next;
-
-	temp->next = new;
-
 	return (new);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: a pointer to the string
+ * Return: the length of a string
+ */
+
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
 }
